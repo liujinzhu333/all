e6463a7eee53def7f49e25fa80b1db93a264f1a0
liujinzhu <liujinzhu@xiaohongshu.net>
2025-04-29 17:27:30 +0800

    项目页面

--- src/views/pages/Projects/Projects.vue
+++ src/views/pages/Projects/Projects.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <div>
-      <el-space wrap
-      :fill-ratio="25"
+      <el-space
+        wrap
+        :fill-ratio="25"
       >
         <el-card  v-for="item in dataList" :span="6" style="min-width: 200px;">
           <el-space>
@@ -14,26 +15,73 @@
             <el-tag v-else-if="item.status === 0" type="info">待开始</el-tag>
             <el-tag v-else-if="item.status === 3" type="warning">暂停</el-tag>
             <el-tag v-else-if="item.status === 4" type="danger">废弃</el-tag>
+            
           </el-space>
           <template #footer>
             <el-space>
-              <el-link v-if="item.url" :href="item.url" target="_blank">演示地址</el-link>
-              <el-link v-if="item.github" :href="item.github" target="_blank">github</el-link>
+              <el-link v-if="item.url" :href="item.url" target="_blank">
+                <el-icon><Link /></el-icon>
+              </el-link>
+              <el-link v-if="item.github" :href="item.github" target="_blank">
+                <img
+                  style="width: 14px;"
+                  src="https://github.githubassets.com/favicons/favicon.svg" alt="github"
+                />
+              </el-link>
+
+              <el-link v-if="item.manage" :href="item.manage" target="_blank">
+                <el-icon><Setting /></el-icon>
+              </el-link>
+              <el-button
+                v-if="canOperate"
+                :icon="Edit"
+                size="small"
+                text
+                circle
+                @click.stop="editProject(item)"
+              />
             </el-space>
           </template>
         </el-card>
+        <div
+          v-if="canOperate"
+          class="add-project"
+          @click="addProject"
+        >
+          添加
+        </div>
       </el-space>
     </div>
+
+    <EditModal
+      v-model:visible="visible"
+      table="project"
+      :data="dataInfo"
+      @confirm="getList"
+    />
   </div>
 </template>
 <script lang="ts" setup>
   import { ref, computed } from 'vue'
-  import manageService from '@/services/manageService';
+  import { Edit, Link, Setting } from '@element-plus/icons-vue'
+  import { canOperate } from '@/config/index'
+  import manageService from '@/services/manageService'
+  import EditModal from '@/components/EditModal.vue'
 
   const dataList = ref<any>([])
+  const visible = ref<boolean>(false)
+  const dataInfo = ref()
   const getList = async () => {
     const data = await manageService.getDataList('project') || []
     dataList.value = data
+  }
+  const addProject = () => {
+    dataInfo.value = {}
+    visible.value = true
+  }
+  const editProject = (item: any) => {
+    dataInfo.value = { ...item }
+    visible.value = true
   }
   getList()
   let data = [
@@ -61,3 +109,16 @@
   ]
 
 </script>
+
+<style>
+.add-project {
+  cursor: pointer;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: #fff;
+}
+</style>
